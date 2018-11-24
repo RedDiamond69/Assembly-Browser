0e12863e92d27bbc5d45726d6f4077f8933fd640
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-24 20:35:06 +0300

    Build method complete.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/TypeBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/TypeBuilder.cs
@@ -20,7 +20,55 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 
         public object Build(Dictionary<string, MethodDeclaration> extMethods)
         {
-
+            string name = _tInfo.Name;
+            string fullName = _tInfo.FullName;
+            string baseType = _tInfo.BaseType?.FullName;
+            bool isInterface = _tInfo.IsInterface;
+            bool isGeneric = _tInfo.IsGenericType;
+            AccessModifiers modifiers = GetModifiers();
+            List<string> implInterfaces = _tInfo.ImplementedInterfaces.Select(type => type.Name).ToList();
+            List<string> genericOptions = new List<string>();
+            if (isGeneric)
+                genericOptions = GetGenericOptions();
+            List<FieldDeclaration> fields = new List<FieldDeclaration>();
+            List<PropertyDeclaration> properties = new List<PropertyDeclaration>();
+            List<MethodDeclaration> methods = new List<MethodDeclaration>();
+            List<EventDeclaration> events = new List<EventDeclaration>();
+            List<TypeDeclaration> nestedTypes = new List<TypeDeclaration>();
+            foreach (MemberInfo member in _tInfo.DeclaredMembers)
+            {
+                if (member is FieldInfo)
+                {
+                    Builder builder = new Builder(new FieldBuilder(member));
+                    fields.Add((FieldDeclaration)builder.Create());
+                }
+                else if (member is PropertyInfo)
+                {
+                    Builder builder = new Builder(new PropertyBuilder(member));
+                    properties.Add((PropertyDeclaration)builder.Create());
+                }
+                else if (member is MethodBase)
+                {
+                    Builder builder = new Builder(new MethodBuilder(member));
+                    methods.Add((MethodDeclaration)builder.Create());
+                }
+                else if (member is EventInfo)
+                {
+                    Builder builder = new Builder(new EventBuilder(member));
+                    events.Add((EventDeclaration)builder.Create());
+                }
+            }
+            foreach (KeyValuePair<string, MethodDeclaration> method in extMethods)
+                if (name == method.Key)
+                    methods.Add(method.Value);
+            foreach (TypeInfo nestedType in _tInfo.DeclaredNestedTypes)
+            {
+                Builder builder = new Builder(new TypeBuilder(nestedType));
+                nestedTypes.Add((TypeDeclaration)builder.Create(extMethods));
+            }
+            return new TypeDeclaration(name, fullName, baseType, isInterface, 
+                isGeneric, modifiers, implInterfaces, genericOptions, 
+                fields, properties, methods, events, nestedTypes);
         }
 
         private List<string> GetGenericOptions()
