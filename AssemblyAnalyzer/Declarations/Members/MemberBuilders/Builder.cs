ffc5e208b3e20b2e10929872bfa6b11ca5e59032
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-24 19:31:00 +0300

    Added implementation for Builder class.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/Builder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/Builder.cs
@@ -8,5 +8,12 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 {
     public class Builder
     {
+        private readonly IMemberBuilder _memberBuilder;
+
+        public Builder(IMemberBuilder memberBuilder) => _memberBuilder = memberBuilder;
+
+        public object Create() => _memberBuilder.Build();
+
+        public object Create(Dictionary<string, MethodDeclaration> extMethods) => ((TypeBuilder)_memberBuilder).Build(extMethods);
     }
 }
