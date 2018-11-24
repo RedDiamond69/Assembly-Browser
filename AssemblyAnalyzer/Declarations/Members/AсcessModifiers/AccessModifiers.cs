8b70cb912424cbc4f4aa22f43712d078d7ec6607
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-24 23:00:06 +0300

    AccessModifiers class implemented.

--- AssemblyAnalyzer/Declarations/Members/AсcessModifiers/AccessModifiers.cs
+++ AssemblyAnalyzer/Declarations/Members/AсcessModifiers/AccessModifiers.cs
@@ -8,5 +8,24 @@ namespace AssemblyAnalyzer.Declarations.Members.AсcessModifiers
 {
     public class AccessModifiers
     {
+        private readonly List<string> _modifiers;
+        private readonly List<string> _sharpModifiers;
+
+        public List<string> Modifiers
+        {
+            get => _modifiers;
+            private set { }
+        }
+        public List<string> SharpModifiers
+        {
+            get => _sharpModifiers;
+            private set { }
+        }
+
+        public AccessModifiers(List<string> modifiers, List<string> sharpModifiers)
+        {
+            Modifiers = modifiers;
+            SharpModifiers = sharpModifiers;
+        }
     }
 }
