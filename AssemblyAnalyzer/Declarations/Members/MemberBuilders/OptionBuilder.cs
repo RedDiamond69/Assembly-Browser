54ee488bdffd3152503d7ff98bc92e204011f064
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 15:59:23 +0300

    Added methods to get modifiers and generic options.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/OptionBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/OptionBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations.Members.AсcessModifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -12,6 +13,29 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
         private readonly ParameterInfo _pInfo;
 
         public OptionBuilder(ParameterInfo parameterInfo) => _pInfo = parameterInfo;
+
+        private AccessModifiers GetModifiers()
+        {
+            List<string> modifiers = new List<string>();
+            List<string> sharpModifiers = new List<string>();
+            ParameterAttributes parameterAttributes = _pInfo.Attributes;
+            modifiers = parameterAttributes.ToString().Split(',').ToList();
+            modifiers = modifiers.Select(str => str.Trim().ToLower()).ToList();
+            if ((parameterAttributes & ParameterAttributes.In) != 0)
+                sharpModifiers.Add("in");
+            if ((parameterAttributes & ParameterAttributes.Out) != 0)
+                sharpModifiers.Add("out");
+            return new AccessModifiers(modifiers, sharpModifiers);
+        }
+
+        private List<string> GetGenericOptions()
+        {
+            List<string> genericOptions = new List<string>();
+            List<Type> genericArguments = _pInfo.ParameterType.GetGenericTypeDefinition().GetGenericArguments().ToList<Type>();
+            foreach (Type type in genericArguments)
+                genericOptions.Add(type.Name);
+            return genericOptions;
+        }
 
         public object Build()
         {
