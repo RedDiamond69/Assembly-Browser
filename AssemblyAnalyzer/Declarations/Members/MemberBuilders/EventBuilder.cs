246c78c799e346582536be7654f96fcadf892604
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 12:04:47 +0300

    Added field and constructor implemented.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/EventBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/EventBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,9 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 {
     public class EventBuilder : IMemberBuilder
     {
-        public EventBuilder()
-        {
-        }
+        private readonly EventInfo _eInfo;
+
+        public EventBuilder(MemberInfo member) => _eInfo = (EventInfo)member;
 
         public object Build()
         {
