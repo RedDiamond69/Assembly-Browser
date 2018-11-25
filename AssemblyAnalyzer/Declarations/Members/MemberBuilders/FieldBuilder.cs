3707c9bbad7ffcdfc2dcc11a12fc47b59b37d530
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 13:21:47 +0300

    GetModifiers method for FieldBuilder implemented.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/FieldBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/FieldBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations.Members.AсcessModifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -38,6 +39,24 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
                     break;
             }
             return accessModifiers;
+        }
+
+        private AccessModifiers GetModifiers()
+        {
+            List<string> modifiers = new List<string>();
+            List<string> sharpModifiers = new List<string>();
+            FieldAttributes fieldAttributes = _fInfo.Attributes;
+            modifiers = fieldAttributes.ToString().Split(',').ToList();
+            modifiers = modifiers.Select(str => str.Trim().ToLower()).ToList();
+            FieldAttributes accessAttributes = fieldAttributes & FieldAttributes.FieldAccessMask;
+            sharpModifiers = GetAccessModifiers(accessAttributes);
+            if ((fieldAttributes & FieldAttributes.Literal) != 0)
+                sharpModifiers.Add("const");
+            if ((fieldAttributes & FieldAttributes.Static) != 0)
+                sharpModifiers.Add("static");
+            if ((fieldAttributes & FieldAttributes.InitOnly) != 0)
+                sharpModifiers.Add("readonly");
+            return new AccessModifiers(modifiers, sharpModifiers);
         }
 
         public object Build()
