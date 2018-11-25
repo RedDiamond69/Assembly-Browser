0b905e92ee803d2460eb079972869a0223e49ba1
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 16:07:13 +0300

    Constructor implemented, added private field.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/PropertyBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/PropertyBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 {
     public class PropertyBuilder
     {
+        private readonly PropertyInfo _propInfo;
+
+        public PropertyBuilder(MemberInfo member) => _propInfo = (PropertyInfo)member;
     }
 }
