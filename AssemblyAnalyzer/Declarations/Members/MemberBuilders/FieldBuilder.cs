a7b8b6f4b97171b2f738523b680908ed9a81286f
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 13:17:29 +0300

    GetAccessModifiers method for FieldBuilder implemented.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/FieldBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/FieldBuilder.cs
@@ -13,6 +13,33 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 
         public FieldBuilder(MemberInfo member) => _fInfo = (FieldInfo)member;
 
+        private List<string> GetAccessModifiers(FieldAttributes attributes)
+        {
+            List<string> accessModifiers = new List<string>();
+            switch (attributes)
+            {
+                case FieldAttributes.Public:
+                    accessModifiers.Add("public");
+                    break;
+                case FieldAttributes.Private:
+                    accessModifiers.Add("private");
+                    break;
+                case FieldAttributes.Assembly:
+                    accessModifiers.Add("internal");
+                    break;
+                case FieldAttributes.Family:
+                    accessModifiers.Add("protected");
+                    break;
+                case FieldAttributes.FamANDAssem:
+                    accessModifiers.Add("private protected");
+                    break;
+                case FieldAttributes.FamORAssem:
+                    accessModifiers.Add("protected internal");
+                    break;
+            }
+            return accessModifiers;
+        }
+
         public object Build()
         {
             throw new NotImplementedException();
