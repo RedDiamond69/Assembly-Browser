e4be01e7727e850cb4a04a01bde5b59cefeebafa
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 14:29:08 +0300

    Added necessary namespace, constructor implemented and added private field.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/MethodBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/MethodBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,9 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 {
     public class MethodBuilder : IMemberBuilder
     {
-        public MethodBuilder()
-        {
-        }
+        private readonly MethodBase _mInfo;
+
+        public MethodBuilder(MemberInfo member) => _mInfo = (MethodBase)member;
 
         public object Build()
         {
