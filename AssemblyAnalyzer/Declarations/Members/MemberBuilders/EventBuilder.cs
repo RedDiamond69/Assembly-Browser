512e8d44afc807a1893c850fc7e6938027f71790
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 12:09:02 +0300

    GetAccessModifiers method in EventBuilder class implemented.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/EventBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/EventBuilder.cs
@@ -13,6 +13,33 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
 
         public EventBuilder(MemberInfo member) => _eInfo = (EventInfo)member;
 
+        private List<string> GetAccessModifiers(MethodAttributes attributes)
+        {
+            List<string> accessModifiers = new List<string>();
+            switch (attributes)
+            {
+                case MethodAttributes.Public:
+                    accessModifiers.Add("public");
+                    break;
+                case MethodAttributes.Private:
+                    accessModifiers.Add("private");
+                    break;
+                case MethodAttributes.Assembly:
+                    accessModifiers.Add("internal");
+                    break;
+                case MethodAttributes.Family:
+                    accessModifiers.Add("protected");
+                    break;
+                case MethodAttributes.FamANDAssem:
+                    accessModifiers.Add("private protected");
+                    break;
+                case MethodAttributes.FamORAssem:
+                    accessModifiers.Add("protected internal");
+                    break;
+            }
+            return accessModifiers;
+        }
+
         public object Build()
         {
             throw new NotImplementedException();
