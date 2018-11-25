2c84030c299903f980e786c592bb67a68a1cdc26
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-25 15:05:05 +0300

    Methods to get modifiers implemented for MethodBuilder.

--- AssemblyAnalyzer/Declarations/Members/MemberBuilders/MethodBuilder.cs
+++ AssemblyAnalyzer/Declarations/Members/MemberBuilders/MethodBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations.Members.AсcessModifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -12,6 +13,53 @@ namespace AssemblyAnalyzer.Declarations.Members.MemberBuilders
         private readonly MethodBase _mInfo;
 
         public MethodBuilder(MemberInfo member) => _mInfo = (MethodBase)member;
+
+        private List<string> GetAccessModifiers(MethodAttributes attributes)
+        {
+            List<string> accessModifiers = new List<string>();
+            switch (attributes)
+            {
+                case MethodAttributes.Public:
+                    accessModifiers.Add("public");
+                    break;
+                case MethodAttributes.Private:
+                    accessModifiers.Add("private");
+                    break;
+                case MethodAttributes.Assembly:
+                    accessModifiers.Add("internal");
+                    break;
+                case MethodAttributes.Family:
+                    accessModifiers.Add("protected");
+                    break;
+                case MethodAttributes.FamANDAssem:
+                    accessModifiers.Add("private protected");
+                    break;
+                case MethodAttributes.FamORAssem:
+                    accessModifiers.Add("protected internal");
+                    break;
+            }
+            return accessModifiers;
+        }
+
+        private AccessModifiers GetModifiers()
+        {
+            List<string> modifiers = new List<string>();
+            List<string> sharpModifiers = new List<string>();
+            MethodAttributes methodAttributes = _mInfo.Attributes & ~MethodAttributes.VtableLayoutMask;
+            modifiers = methodAttributes.ToString().Split(',').ToList();
+            modifiers = modifiers.Select(str => str.Trim().ToLower()).ToList();
+            MethodAttributes accessAttributes = methodAttributes & MethodAttributes.MemberAccessMask;
+            sharpModifiers = GetAccessModifiers(accessAttributes);
+            if ((methodAttributes & MethodAttributes.Abstract) != 0)
+                sharpModifiers.Add("abstract");
+            if ((methodAttributes & MethodAttributes.Final) != 0)
+                sharpModifiers.Add("sealed");
+            if ((methodAttributes & MethodAttributes.Virtual) != 0)
+                sharpModifiers.Add("virtual");
+            if ((methodAttributes & MethodAttributes.Static) != 0)
+                sharpModifiers.Add("static");
+            return new AccessModifiers(modifiers, sharpModifiers);
+        }
 
         public object Build()
         {
