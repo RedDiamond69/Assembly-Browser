29e1756ebc3db52afcda632fd1291ea589dcad71
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-28 19:09:02 +0300

    NamespaceDeclaration class implemented.

--- AssemblyAnalyzer/Declarations/NamespaceDeclaration.cs
+++ AssemblyAnalyzer/Declarations/NamespaceDeclaration.cs
@@ -8,5 +8,25 @@ namespace AssemblyAnalyzer.Declarations
 {
     public class NamespaceDeclaration
     {
+        private readonly List<TypeDeclaration> _typeDeclarations;
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public List<TypeDeclaration> TypeDeclarations
+        {
+            get => _typeDeclarations;
+            private set { }
+        }
+
+        public void AddType(TypeDeclaration typeDeclaration) => _typeDeclarations.Add(typeDeclaration);
+
+        public NamespaceDeclaration(string name)
+        {
+            Name = name;
+            _typeDeclarations = new List<TypeDeclaration>();
+        }
     }
 }
