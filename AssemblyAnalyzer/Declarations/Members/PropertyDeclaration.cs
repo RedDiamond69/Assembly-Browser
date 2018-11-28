8ec0f3568805541d63bdd8ac2b287b1e5c7fe26d
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-28 17:34:14 +0300

    PropertyDeclaration was implemented.

--- AssemblyAnalyzer/Declarations/Members/PropertyDeclaration.cs
+++ AssemblyAnalyzer/Declarations/Members/PropertyDeclaration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations.Members.AсcessModifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,57 @@ using System.Threading.Tasks;
 
 namespace AssemblyAnalyzer.Declarations.Members
 {
-    public class PropertyDeclaration
+    public class PropertyDeclaration : IMember
     {
+        private readonly AccessModifiers _accessModifiers;
+        private readonly List<string> _genericalParams;
+
+        public bool IsGenerical
+        {
+            get;
+            private set;
+        }
+        public bool CanBeRead
+        {
+            get;
+            private set;
+        }
+        public bool CanBeWrite
+        {
+            get;
+            private set;
+        }
+        public AccessModifiers AccessModifiers
+        {
+            get => _accessModifiers;
+            private set { }
+        }
+        public string TypeName
+        {
+            get;
+            private set;
+        }
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public IEnumerable<string> GenericalParams
+        {
+            get => _genericalParams;
+            private set { }
+        }
+
+        public PropertyDeclaration(string name, string tName, bool isGenerical, bool canBeRead, bool canBeWrite, 
+            AccessorsModifiers accessorsModifiers, List<string> genericalParams)
+        {
+            Name = name;
+            TypeName = tName;
+            IsGenerical = isGenerical;
+            CanBeRead = canBeRead;
+            CanBeWrite = canBeWrite;
+            AccessModifiers = accessorsModifiers;
+            GenericalParams = genericalParams;
+        }
     }
 }
