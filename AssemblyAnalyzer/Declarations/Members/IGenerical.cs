44d27b36ba4b4277f65bfa86c20a4f838811959d
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-28 16:14:13 +0300

    IGenerical implemented.

--- AssemblyAnalyzer/Declarations/Members/IGenerical.cs
+++ AssemblyAnalyzer/Declarations/Members/IGenerical.cs
@@ -8,6 +8,7 @@ namespace AssemblyAnalyzer.Declarations.Members
 {
     public interface IGenerical
     {
-
+        bool IsGenerical { get; }
+        List<string> GenericalParams { get; }
     }
 }
