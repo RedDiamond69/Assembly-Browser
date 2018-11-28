af0df563e46ca5f52f92a6a3da64b07565a3a0b7
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-28 15:57:04 +0300

    IMember interface implemented.

--- AssemblyAnalyzer/Declarations/Members/IMember.cs
+++ AssemblyAnalyzer/Declarations/Members/IMember.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations.Members.AсcessModifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace AssemblyAnalyzer.Declarations.Members
 {
     public interface IMember
     {
+        string Name { get; }
+        AccessModifiers AccessModifiers { get; }
     }
 }
