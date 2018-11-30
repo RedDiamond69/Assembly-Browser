1828c4162d080aaef223c0f8d6d08e21292da491
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-30 20:38:14 +0300

    Added methods for get presentation and modifiers for methods. MethodVM class implemented.

--- AssemblyBrowser/ViewModel/MethodVM.cs
+++ AssemblyBrowser/ViewModel/MethodVM.cs
@@ -11,6 +11,51 @@ namespace AssemblyBrowser.ViewModel
     {
         private readonly MethodDeclaration _methodDeclaration;
 
+        public string StringPresentation
+        {
+            get => GetPresentation();
+            private set { }
+        }
+
+        private string GetPresentation()
+        {
+            string stringPresentation = String.Empty;
+            stringPresentation = String.Format("{0} {1} {2}", 
+                GetAccessModifiers(_methodDeclaration.AccessModifiers.SharpModifiers), 
+                _methodDeclaration.RetTypeName, _methodDeclaration.Name);
+            if (_methodDeclaration.IsGenerical)
+                stringPresentation += String.Format("<{0}>", GetAccessModifiers(_methodDeclaration.GenericalParams));
+            stringPresentation += String.Format("({0}) {1}", GetOptions(_methodDeclaration.Options), 
+                _methodDeclaration.IsExtention ? "extension" : "");
+            return stringPresentation;
+        }
+
+        public string GetAccessModifiers(List<string> accessModifiers)
+        {
+            string modifiersPresentation = String.Empty;
+            foreach (string modifier in accessModifiers)
+                modifiersPresentation += String.Format("{0} ", modifier);
+            return modifiersPresentation.TrimEnd();
+        }
+
+        private string GetOptions(List<OptionDeclaration> options)
+        {
+            string optionPresentation = String.Empty;
+            foreach (OptionDeclaration option in options)
+            {
+                string optionName = String.Empty;
+                if (option.IsGenerical)
+                    optionName = String.Format("{0}<{1}> {2}", option.TypeName, 
+                        GetAccessModifiers(option.GenericalParams), option.Name);
+                else
+                    optionName = String.Format("{0} {1}", option.TypeName, option.Name);
+                optionPresentation += String.Format("{0} {1}, ", GetAccessModifiers(option.AccessModifiers.SharpModifiers), 
+                    optionName).TrimStart();
+            }
+            return optionPresentation.Length > 2 ? 
+                optionPresentation.Substring(0, optionPresentation.Length - 2) : optionPresentation;
+        }
+
         public MethodVM(MethodDeclaration methodDeclaration)
         {
             _methodDeclaration = methodDeclaration;
