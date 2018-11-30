d2c0244716858b9d63ad37625b78f8a045b806cd
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-30 20:11:26 +0300

    Added methods for get and present modifiers for accessors. PropertyVM class implemented.

--- AssemblyBrowser/ViewModel/PropertyVM.cs
+++ AssemblyBrowser/ViewModel/PropertyVM.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AssemblyAnalyzer.Declarations.Members;
+using AssemblyAnalyzer.Declarations.Members.AсcessModifiers;
 
 namespace AssemblyBrowser.ViewModel
 {
@@ -19,7 +20,33 @@ namespace AssemblyBrowser.ViewModel
 
         private string GetPresentation()
         {
-            throw new NotImplementedException();
+            string accessModifiers = GetAccessModifiers(_propertyDeclaration.AccessModifiers.SharpModifiers);
+            string tName = _propertyDeclaration.TypeName;
+            string name = _propertyDeclaration.Name;
+            string accessors = GetAccessors();
+            if (_propertyDeclaration.IsGenerical)
+                tName += string.Format("<{0}>", GetAccessModifiers((List<string>)_propertyDeclaration.GenericalParams));
+            return string.Format("{0} {1} {2} {3}", accessModifiers, tName, name, accessors).Trim();
+        }
+
+        private string GetAccessors()
+        {
+            string modifiersPresentation = "{ ";
+            AccessorsModifiers accessorsModifiers = (AccessorsModifiers)_propertyDeclaration.AccessModifiers;
+            if (_propertyDeclaration.CanBeRead)
+                modifiersPresentation += string.Format("{0} get; ", GetAccessModifiers(accessorsModifiers.Getters)).TrimStart();
+            if (_propertyDeclaration.CanBeWrite)
+                modifiersPresentation += string.Format("{0} set; ", GetAccessModifiers(accessorsModifiers.Setters)).TrimStart();
+            modifiersPresentation += "}";
+            return modifiersPresentation;
+        }
+
+        private string GetAccessModifiers(List<string> accessModifiers)
+        {
+            string modifiersPresentation = String.Empty;
+            foreach (string modifier in accessModifiers)
+                modifiersPresentation += string.Format("{0} ", modifier);
+            return modifiersPresentation.TrimEnd();
         }
 
         public PropertyVM(PropertyDeclaration property)
