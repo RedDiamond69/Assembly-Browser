d58782e938e69a7ee0e729ce2653b3c1ffd02fa9
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-30 19:01:28 +0300

    Added private field and empty constructor for NamespaceVM class

--- AssemblyBrowser/ViewModel/NamespaceVM.cs
+++ AssemblyBrowser/ViewModel/NamespaceVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace AssemblyBrowser.ViewModel
 {
     public class NamespaceVM
     {
+        private readonly NamespaceDeclaration _namespaceDeclaration;
+
+        public NamespaceVM()
+        {
+        }
     }
 }
