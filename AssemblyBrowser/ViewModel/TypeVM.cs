9a66b001c0f4286c63e64d6063bdb1366ce16ff1
Alexey Kosyuk <reddiamond821@gmail.com>
2018-11-30 19:35:15 +0300

    Added methods for get access modifiers and to get presentation.

--- AssemblyBrowser/ViewModel/TypeVM.cs
+++ AssemblyBrowser/ViewModel/TypeVM.cs
@@ -11,6 +11,36 @@ namespace AssemblyBrowser.ViewModel
     {
         private readonly TypeDeclaration _typeDeclaration;
 
+        public string StringPresentation
+        {
+            get => GetPresentation();
+            private set { }
+        }
+
+        private string GetPresentation()
+        {
+            string presentation = String.Empty;
+            string keyword = _typeDeclaration.IsInterface ? "interface" : "class";
+            presentation = string.Format("{0} {1} {2}", GetAccessModifiers(_typeDeclaration.AccessModifiers.SharpModifiers), 
+                keyword, _typeDeclaration.Name);
+            if (_typeDeclaration.IsGenerical)
+                presentation += string.Format("<{0}>", GetAccessModifiers(_typeDeclaration.GenericalParams));
+            if (_typeDeclaration.BaseType != null)
+                presentation += string.Format(" : {0} ", _typeDeclaration.BaseType.Split('.').Last());
+            if (_typeDeclaration.ImplementedInterfaces.Count() > 0)
+                foreach (string implementInterface in _typeDeclaration.ImplementedInterfaces)
+                    presentation += string.Format("{0} ", implementInterface);
+            return presentation;
+        }
+
+        private string GetAccessModifiers(List<string> accessModifiers)
+        {
+            string modifiersPresentation = String.Empty;
+            foreach (string modifier in accessModifiers)
+                modifiersPresentation += String.Format("{0} ", modifier);
+            return modifiersPresentation.TrimEnd();
+        }
+
         public TypeVM(TypeDeclaration typeDeclaration)
         {
             _typeDeclaration = typeDeclaration;
