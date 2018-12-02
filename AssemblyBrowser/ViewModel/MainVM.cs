240ef436d676cd34975019bb32737ec967216be3
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-02 16:06:42 +0300

    MainVM implemented. View model completed.

--- AssemblyBrowser/ViewModel/MainVM.cs
+++ AssemblyBrowser/ViewModel/MainVM.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using AssemblyBrowser.Model;
+using AssemblyBrowser.Command;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AssemblyBrowser.ViewModel
 {
-    public class MainVM
+    public class MainVM : INotifyPropertyChanged
     {
+        private AssemblyBrowserModel _model;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AssemblyVM AssemblyVM
+        {
+            get => _model.AssemblyVM;
+            private set { }
+        }
+        public Commands OpenAssemblyCommand { get; set; }
+
+        public MainVM()
+        {
+            _model = new AssemblyBrowserModel();
+            OpenAssemblyCommand = new Commands(OpenAssembly);
+            _model.PropertyChanged += (s, e) => RaisePropertyChanged(e.PropertyName);
+        }
+
+        public void OpenAssembly(object param)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog().Value)
+                _model.Open(open.FileName);
+        }
+
+        public void RaisePropertyChanged(string property) => PropertyChanged?.Invoke(this, 
+            new PropertyChangedEventArgs(property));
     }
 }
