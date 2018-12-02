209d78f07b73e3cfa6d8ca4b3729deadc8930709
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-02 15:33:02 +0300

    INotifyPropertyChanged interface inherited, added empty implementation.

--- AssemblyBrowser/Model/AssemblyBrowserModel.cs
+++ AssemblyBrowser/Model/AssemblyBrowserModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AssemblyBrowser.Model
 {
-    public class AssemblyBrowserModel
+    public class AssemblyBrowserModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
