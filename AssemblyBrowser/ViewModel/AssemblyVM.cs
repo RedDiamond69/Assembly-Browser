f192b8ec5bbdeb1c44b2e2074ec3bc412d815789
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-02 15:26:31 +0300

    Assembly view model implemented.

--- AssemblyBrowser/ViewModel/AssemblyVM.cs
+++ AssemblyBrowser/ViewModel/AssemblyVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AssemblyAnalyzer.Declarations;
+using AssemblyAnalyzer.Info;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace AssemblyBrowser.ViewModel
 {
     public class AssemblyVM
     {
+        private readonly AssemblyInfo _assemblyInfo;
+
+        public string StringPresentation
+        {
+            get => String.Empty;
+            private set { }
+        }
+        public IEnumerable<NamespaceVM> DeclaratedNamespaces
+        {
+            get
+            {
+                foreach(NamespaceDeclaration namespaceDeclaration in _assemblyInfo.NamespaceDeclarations)
+                    yield return new NamespaceVM(namespaceDeclaration);
+            }
+            private set { }
+        }
+
+        public AssemblyVM(AssemblyInfo assemblyInfo) => _assemblyInfo = assemblyInfo;
     }
 }
